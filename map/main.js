c74245a15b45af68c55c7aafab53afc9f0c7d23b
liturner <turnertech@outlook.de>
2023-05-07 18:25:16 +0200

    Map improvements

--- map/main.js
+++ map/main.js
@@ -7,14 +7,14 @@ class OptaStyle {
 
   static imageSrc(opta) {
     const imageSrcMap = {
-      2110: 'tz/thw-ztrfu.png',
-      2200: 'tz/thw-b.png'
+      2110: 'map/tz/thw-ztrfu.png',
+      2200: 'map/tz/thw-b.png'
     };
 
     const id = opta.substring(13, 17);
     let imageSrc = imageSrcMap[id];
     if (!imageSrc) {
-      imageSrc = 'tz/thw-helfer.png'
+      imageSrc = 'map/tz/thw-helfer.png'
     }
     return imageSrc;
   }
@@ -57,12 +57,17 @@ class OptaStyle {
 class HazardStyle {
   static imageSrc(hazardType) {
     const imageSrcMap = {
-      "hazard-acute": 'tz/gefahr-acute.png'
+      "thw-ztrfu": 'map/tz/thw-ztrfu.png',
+      "thw-helfer": 'map/tz/thw-helfer.png',
+      "fw-abc-erkundung": 'map/tz/fw-abc-erkundung.png',
+      "hazard-acute": 'map/tz/gefahr-acute.png',
+      "gefahr-gs": 'map/tz/gefahr-gs.png',
+      "gefahr-vermutet-strom": 'map/tz/gefahr-vermutet-strom.png'
     };
 
     let imageSrc = imageSrcMap[hazardType];
     if (!imageSrc) {
-      imageSrc = 'tz/gefahr-acute.png'
+      imageSrc = 'map/tz/gefahr-acute.png'
     }
     return imageSrc;
   }
@@ -127,21 +132,6 @@ class AreaStyle {
   };
 }
 
-const basemap = new ol.layer.Tile({
-  opacity: 1,
-  source: new ol.source.XYZ({
-    attributions:
-      '<a href="https://www.bkg.bund.de">Kartendarstellung: © Bundesamt für Kartographie und Geodäsie (2021)</a>, ' +
-      '<a href="https://sg.geodatenzentrum.de/web_public/Datenquellen_TopPlus_Open.pdf">Datenquellen</a>',
-    url: 'https://mapproxy.turnertech.de/tms/1.0.0/top_plus_open/webmercator/{z}/{x}/{-y}.png',
-    //projection: ol.proj.proj4.fromEPSGCode(3857),
-    tileGrid: new ol.tilegrid.TileGrid({
-      extent: [-20037508.34, -20037508.34, 20037508.34, 20037508.34],
-      resolutions: [156543.033928041, 78271.5169640205, 39135.7584820102, 19567.8792410051, 9783.93962050256, 4891.96981025128, 2445.98490512564, 1222.99245256282, 611.496226281410, 305.748113140705, 152.874056570353, 76.4370282851763, 38.2185141425881, 19.1092570712941, 9.55462853564703, 4.77731426782352, 2.38865713391176, 1.19432856695588, 0.597164283477939]
-    })
-  }),
-});
-
 const onlineBasemap = new ol.layer.Tile({
   opacity: 0.7,
   source: new ol.source.WMTS({
@@ -164,14 +154,6 @@ const copWfsSource = new ol.source.Vector({
   format: new ol.format.GML32({
     srsName: 'http://www.opengis.net/def/crs/EPSG/0/4326'
   }),
-  url: function (extent) {
-    return (
-      '/ows?SERVICE=WFS&VERSION=2.0.2&REQUEST=GetFeature&TYPENAMES=boscop:Area&' +
-      'SRSNAME=http://www.opengis.net/def/crs/EPSG/0/4326&BBOX=' +
-      ol.proj.transformExtent(extent, 'EPSG:3857','EPSG:4326').join(',') +
-      ',http://www.opengis.net/def/crs/EPSG/0/4326'
-    );
-  },
   strategy: ol.loadingstrategy.bbox
 });
 
@@ -179,14 +161,6 @@ const unitSource = new ol.source.Vector({
   format: new ol.format.GML32({
     srsName: 'http://www.opengis.net/def/crs/EPSG/0/4326'
   }),
-  url: function (extent) {
-    return (
-      '/ows?SERVICE=WFS&VERSION=2.0.2&REQUEST=GetFeature&TYPENAMES=boscop:Unit&' +
-      'SRSNAME=http://www.opengis.net/def/crs/EPSG/0/4326&BBOX=' +
-      ol.proj.transformExtent(extent, 'EPSG:3857','EPSG:4326').join(',') +
-      ',http://www.opengis.net/def/crs/EPSG/0/4326'
-    );
-  },
   strategy: ol.loadingstrategy.bbox
 });
 
@@ -194,33 +168,26 @@ const hazardSource = new ol.source.Vector({
   format: new ol.format.GML32({
     srsName: 'http://www.opengis.net/def/crs/EPSG/0/4326'
   }),
-  url: function (extent) {
-    return (
-      '/ows?SERVICE=WFS&VERSION=2.0.2&REQUEST=GetFeature&TYPENAMES=boscop:Hazard&' +
-      'SRSNAME=http://www.opengis.net/def/crs/EPSG/0/4326&BBOX=' +
-      ol.proj.transformExtent(extent, 'EPSG:3857','EPSG:4326').join(',') +
-      ',http://www.opengis.net/def/crs/EPSG/0/4326'
-    );
-  },
   strategy: ol.loadingstrategy.bbox
 });
 
 const copLayer = new ol.layer.Vector({
-  //source: copWfsSource,
+  source: copWfsSource,
   style: AreaStyle.styleFunction
 });
 
 const unitLayer = new ol.layer.Vector({
-  //source: unitSource,
+  source: unitSource,
   style: OptaStyle.styleFunction
 });
 
 const hazardLayer = new ol.layer.Vector({
-  //source: hazardSource,
+  source: hazardSource,
   style: HazardStyle.styleFunction
 });
 
 const map = new ol.Map({
+  controls: ol.control.defaults.defaults().extend([new ol.control.FullScreen(), new ol.control.ScaleLine()]),
   target: 'map',
   layers: [
     onlineBasemap,
@@ -229,90 +196,71 @@ const map = new ol.Map({
     hazardLayer
   ],
   view: new ol.View({
-    center: [1000000, 6650300],
-    zoom: 6
+    center: [1244800, 6230600],
+    zoom: 17
   })
 });
 
 const selectSingleClick = new ol.interaction.Select({
   condition: ol.events.condition.pointerMove
 });
-map.addInteraction(selectSingleClick);
-
-function refreshCopLayer() {
-  copLayer.getSource().refresh();
-  unitLayer.getSource().refresh();
-  hazardLayer.getSource().refresh();
-}
+//map.addInteraction(selectSingleClick);
 
 let draw;
 document.getElementById('insert').addEventListener('click', function () {
+  if(draw) {
+    map.removeInteraction(draw);
+  }
   draw = new ol.interaction.Draw({
     source: copWfsSource,
     type: 'Polygon'
   });
   draw.on('drawend', function (e) {
-    const formatWFS = new ol.format.WFS({
-      version: '2.0.0',
-      featureNS: 'urn:ns:de:turnertech:boscop'
-    });
     e.feature.setProperties({
       "areaType": typeSelect.value
     });
-    e.feature.getGeometry().transform('EPSG:3857', 'EPSG:4326');
-    const node = formatWFS.writeTransaction([e.feature], null, null, {
-      featureNS: 'urn:ns:de:turnertech:boscop',
-      featurePrefix: 'boscop',
-      featureType: 'Area',
-      srsName: 'http://www.opengis.net/def/crs/EPSG/0/4326',
-      version: '2.0.0',
-      gmlOptions: {
-        featureNS: 'urn:ns:de:turnertech:boscop',
-        featureType: 'Area',
-        srsName: 'http://www.opengis.net/def/crs/EPSG/0/4326'
-      }
-    });
-    e.feature.getGeometry().transform('EPSG:4326', 'EPSG:3857');
-    const xs = new XMLSerializer();
-    const payload = xs.serializeToString(node);
-    console.log(payload);
-    map.removeInteraction(draw);
+    //map.removeInteraction(draw);
   });
   map.addInteraction(draw);
 });
 
-
 document.getElementById('insertHazard').addEventListener('click', function () {
+  if(draw) {
+    map.removeInteraction(draw);
+  }
   draw = new ol.interaction.Draw({
     source: hazardSource,
     type: 'Point'
   });
   draw.on('drawend', function (e) {
-    const formatWFS = new ol.format.WFS({
-      version: '2.0.0',
-      featureNS: 'urn:ns:de:turnertech:boscop'
-    });
     e.feature.setProperties({
       "hazardType": hazardSelect.value
     });
-    e.feature.getGeometry().transform('EPSG:3857', 'EPSG:4326');
-    const node = formatWFS.writeTransaction([e.feature], null, null, {
-      featureNS: 'urn:ns:de:turnertech:boscop',
-      featurePrefix: 'boscop',
-      featureType: 'Hazard',
-      srsName: 'http://www.opengis.net/def/crs/EPSG/0/4326',
-      version: '2.0.0',
-      gmlOptions: {
-        featureNS: 'urn:ns:de:turnertech:boscop',
-        featureType: 'Hazard',
-        srsName: 'http://www.opengis.net/def/crs/EPSG/0/4326'
-      }
-    });
-    e.feature.getGeometry().transform('EPSG:4326', 'EPSG:3857');
-    const xs = new XMLSerializer();
-    const payload = xs.serializeToString(node);
-    console.log(payload);
-    map.removeInteraction(draw);
+    //map.removeInteraction(draw);
   });
   map.addInteraction(draw);
+});
+
+document.getElementById('insertUnit').addEventListener('click', function () {
+  if(draw) {
+    map.removeInteraction(draw);
+  }
+  draw = new ol.interaction.Draw({
+    source: hazardSource,
+    type: 'Point'
+  });
+  draw.on('drawend', function (e) {
+    e.feature.setProperties({
+      "hazardType": unitSelect.value
+    });
+    //map.removeInteraction(draw);
+  });
+  map.addInteraction(draw);
+});
+
+document.addEventListener("keyup", (event) => {
+  if (event.isComposing || event.keyCode === 229) {
+    return;
+  }
+  map.removeInteraction(draw);
 });
